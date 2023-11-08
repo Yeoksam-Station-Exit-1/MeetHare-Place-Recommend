60ff7e504617376f0479edff4c715688c2cec16f
chickennight <112360329+chickennight@users.noreply.github.com>
2023-11-08 16:41:31 +0900

    Update SeleniumService.java

--- src/main/java/yeoksamstationexit1/recommend/service/SeleniumService.java
+++ src/main/java/yeoksamstationexit1/recommend/service/SeleniumService.java
@@ -10,25 +10,21 @@ import yeoksamstationexit1.recommend.util.Selenium;
 
 import java.util.List;
 
-@Transactional
 @RequiredArgsConstructor
 @Service
 public class SeleniumService {
     private final StationRepository stationRepository;
     private final Selenium selenium;
 
-    @Scheduled(cron = "0 45 16 * * 1", zone = "Asia/Seoul") // 매주 월요일 0시 0분 0초
+    @Scheduled(cron = "0 0 0 1 * ?", zone = "Asia/Seoul") // 매달 1일 0시 0분 0초
     public void createPlace() throws InterruptedException {
         List<Station> stations = stationRepository.findAll();
 
-//        for(Station station : stations) {
-        for(int i=827; i<stations.size(); i++) {
-//            System.out.println(station.getName());
-//            selenium.createPlaceDetail(station, "restaurant"); // 카테고리 차후 수정
-            selenium.createPlaceDetail(stations.get(i), "restaurant");
-            selenium.createPlaceDetail(stations.get(i), "activity");
-            selenium.createPlaceDetail(stations.get(i), "study");
-            selenium.createPlaceDetail(stations.get(i), "culture");
+        for(Station station : stations) {
+            selenium.createPlaceDetail(station, "restaurant");
+            selenium.createPlaceDetail(station, "activity");
+            selenium.createPlaceDetail(station, "study");
+            selenium.createPlaceDetail(station, "culture");
         }
     }
 }
